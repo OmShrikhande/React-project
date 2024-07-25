0f05d5d6665eed84a86f0f60667c8261f22e50f6
jis24 <165779138+orscoder@users.noreply.github.com>
2024-07-25 15:37:29 +0530

    hooks data added

--- src/data.js
+++ src/data.js
@@ -40,23 +40,23 @@ export const Example ={
   },
   jsx :{
     title: 'JSX',
-    description: 'This is an example of a React component',
+    description: 'This is an example of a React Jsx',
     code : `function Welcome(){
-      return <h1> Components </h1>
+      return <h1> Jsx</h1>
     }`,
   },
   childrenprop :{
     title: 'Props',
-    description: 'This is an example of a React component',
+    description: 'This is an example of a React Props, you are so sweet you are kind, u are talking skills are good. i love you, My heart is beating for you',
     code : `function Welcome(){
-      return <h1> Components </h1>
+      return <h1> props </h1>
     }`,
   },
   state :{
     title: 'State',
-    description: 'This is an example of a React component',
+    description: 'Kuch logo se baat band ho jaati hai pr ye ittefak kum nhi hote,Ristoon ke gehraiya num ho jaati pr aaj bhi khafa rehte haikehne ko shayad bahot kuch hota hai pr aab lafz nahi bachte, Himmat nahi hoti kyu ki aankhe milti to hai pr aab theer nahi paati kuch logo ke sath itne pal bitaye hote hai pr aab vo anjane lagne lagte hai ye toote rishte aaj bhi soch mein daal dete hai ke waqt ke saath jasbaat kaise badal jaate hai.',
     code : `function Welcome(){
-      return <h1> Components </h1>
+      return <h1> State </h1>
     }`,
   }
 }
